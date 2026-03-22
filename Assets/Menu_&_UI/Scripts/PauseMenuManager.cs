75195992f6fe9c4965850471cc08ee0c5ecd6420
Jashby <jtashby@gmail.com>
2026-03-22 17:13:48 +1030

    Update PauseMenuManager.cs
    pause scripts and new demo scene for pause menu additions

--- Assets/Menu_&_UI/Scripts/PauseMenuManager.cs
+++ Assets/Menu_&_UI/Scripts/PauseMenuManager.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class PauseMenuManager : MonoBehaviour
 {
+
+
+    public GameObject PausePanel;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Pause()
+    {
+        PausePanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void Continue()
+    {
+        PausePanel.SetActive(false);
+        Time.timeScale = 1;
+
     }
 }
