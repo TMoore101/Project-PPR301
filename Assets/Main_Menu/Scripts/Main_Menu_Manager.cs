1a24a17e9f14749f98fdba3e448878ac4690715b
Jashby <jtashby@gmail.com>
2026-03-22 15:42:22 +1030

    Basic play button implementation done

---
+++ Assets/Main_Menu/Scripts/Main_Menu_Manager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_Manager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+        
+    }
+}
